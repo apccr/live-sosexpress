fe7597b6e1e88da958b92bc04647f7d37826757b
ARX <adriancamposcr@gmail.com>
2026-05-09 09:46:44 -0600

    Logos n credits

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1777915570|2597767748';
+const CACHE_VERSION = '1778341271|1532925874';
 /** @type {string} */
 const CACHE_PREFIX = 'Aritmetia - SOS -sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
